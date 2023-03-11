93627b498df5428e1f8db802135f15eba0e4c0e4
matthewmontiel <xoptimuszerox@gmail.com>
2023-03-11 17:34:24 -0600

    added starter code and pushed some JS

---
+++ assets/script.js
@@ -0,0 +1,48 @@
+// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
+// the code isn't run until the browser has finished rendering all the elements
+// in the html.
+const localeSettings = {};
+dayjs.locale(localeSettings);
+
+$(function () {
+  const timeNow = dayjs().format("H");
+  function changetimeColor() {
+    $(".time-block").each(function () {
+      const timePeriod = parseInt(this.id);
+      $(this).toggleClass("past", timePeriod < timeNow);
+      $(this).toggleClass("present", timePeriod === timeNow);
+      $(this).toggleClass("future", timePeriod > timeNow);
+    });
+  }
+
+  function enterText() {
+    $(".saveBtn").on("click", function () {
+      const token = $(this).parent().attr("id");
+      const points = $(this).siblings(".description").val();
+      localStorage.setItem(token, points);
+    });
+  }
+
+  function revaluatetimeColor() {
+    $(".time-block").each(function () {
+      const timePeriod = parseInt(this.id);
+      if (timePeriod == timeNow) {
+        $(this).removeClass("past future").addClass("present");
+      } else if (timePeriod < timeNow) {
+        $(this).removeClass("future present").addClass("past");
+      } else {
+        $(this).removeClass("past present").addClass("future");
+      }
+    });
+  }
+
+  $(".time-block").each(function () {
+    const token = $(this).attr("id");
+    const points = localStorage.getItem(token);
+    $(this).children(".description").val(points);
+  });
+
+  changetimeColor();
+  enterText();
+  revaluatetimeColor();
+});
