25ddb6a6185e8939809701ad037fe0b878ea85ea
matthewmontiel <xoptimuszerox@gmail.com>
2023-03-11 18:26:26 -0600

    updated time function

--- assets/script.js
+++ assets/script.js
@@ -44,11 +44,15 @@ $(function () {
 
   function updateTime() {
     const dateElement = $("#date");
-    const currentDate = dayjs.format("dddd, MMMM D, YYYY");
+    const timeElement = $("#time")
+    const currentDate = dayjs().format("dddd, MMMM D, YYYY");
+    const currentTime = dayjs().format('hh:mm:ss A');
     dateElement.text(currentDate);
+    timeElement.text(currentTime);
   }
 
   changetimeColor();
   enterText();
   revaluatetimeColor();
+  setInterval(updateTime, 1000);
 });
