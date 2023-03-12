6abac8948b934c8a75bb96d6dbbc0112ec1ec4ff
matthewmontiel <xoptimuszerox@gmail.com>
2023-03-12 17:20:42 -0500

    fixed the 'th' and 'rd' for the time numbers.

--- assets/script.js
+++ assets/script.js
@@ -46,7 +46,7 @@ $(function () {
   function updateTime() {
     const dateElement = $("#date");
     const timeElement = $("#time");
-    const currentDate = dayjs().format("dddd MMMM D, YYYY");
+    const currentDate = dayjs().format("dddd MMMM Do, YYYY");
     const currentTime = dayjs().format("hh:mm:ss A");
     dateElement.text(currentDate);
     timeElement.text(currentTime);
